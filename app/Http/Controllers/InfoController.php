40fb9da00b7222e48189ac168c28a5fae9d299ee
Nohaaa <nogaaa16@gmail.com>
2020-01-10 11:35:21 +0200

    four

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -39,7 +39,8 @@ class InfoController extends Controller
         return response()->json(
             $Ids
         )->header('Access-Control-Allow-Origin', '*')->header('Accept', 'application/json')->header('Content-Type', 'application/json')
-            ->header('Access-Control-Allow-Methods', 'GET, POST')->header('Access-Control-Allow-Headers', 'X-Requested-With');
+            ->header('Access-Control-Allow-Methods', 'GET, POST')->header('Access-Control-Allow-Headers', '*')
+            ->header('Access-Control-Allow-Credentials', 'true') ->header('Access-Control-Max-Age', '86400');
 
     }
 
